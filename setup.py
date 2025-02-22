6dcaa800b6fd0ed09c029edd6f2febe9f930162f
Ben DeMouth <165355226+bendemouth@users.noreply.github.com>
2025-02-22 10:52:12 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='BracketDrawer',
-    version='0.1.1',
+    version='0.1.2',
     author="Ben DeMouth",
     author_email="bendemouth@gmail.com",
     url="https://github.com/bendemouth/BracketDrawer",
